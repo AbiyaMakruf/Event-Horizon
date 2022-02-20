e656b73ddda576fc70faedf12e98e35edd40577b
Abiya Makruf <abiyamf@student.telkomuniversity.ac.id>
2022-02-20 22:30:28 +0700

    Menambah .gitignore, Mengubah css error 404 menjadi 1file html, Membuat /order

---
+++ order/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Horizon Formulir</title>
+
+    <!-- Css -->
+    <link rel="stylesheet" href="assets/css/nicepage.css" media="screen">
+    <link rel="stylesheet" href="assets/css/FORM-SUBMIT.css" media="screen">
+
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Formulir Order">
+    <meta property="og:type" content="website">
+
+    <!-- Fav Icon -->
+    <meta property="og:image" content="../assets/images/favicon.png" href="../assets/images/favicon.png" >
+    <link rel="shortcut icon" type="image/png" href="../assets/images/favicon.png">
+    <link rel="icon" type="image/png" href="../assets/images/favicon.png">
+    <link rel="favicon" type="image/png" href="../assets/images/favicon.png">
+    <link itemprop="thumbnailUrl" href="../assets/images/favicon.png"> <span itemprop="thumbnail" itemscope itemtype="http://schema.orgImageObject"> <link itemprop="url" href="../assets/images/favicon.png"> </span>
+
+    <!-- Java Script -->
+    <script class="u-script" type="text/javascript" src="assets/js/jquery.js" ></script>
+    <!-- <script class="u-script" type="text/javascript" src="assets/js/nicepage.js" ></script> -->
+
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="FORM SUBMIT">
+    <meta property="og:type" content="website">
+
+</head>
+<body class="u-body u-xl-mode">
+  <section class="u-clearfix u-custom-color-4 u-section-1" id="carousel_df2b">
+    <div class="u-custom-color-4 u-expanded-width u-shape u-shape-rectangle u-shape-1"></div>
+    <div class="u-clearfix u-gutter-36 u-layout-wrap u-layout-wrap-1">
+      <div class="u-layout" >
+        <div class="u-layout-row" >
+          <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-1">
+            <div class="u-container-layout u-container-layout-1">
+              <h3 class="u-text u-text-default u-text-1">FORMULIR ORDER</h3>
+              <h6 class="u-custom-font u-text u-text-font u-text-2">Mohon isi email &amp; username sesuai dengan akun yang anda buat di the-eventhorizon.com dan beberapa kotak lainnya sesuai dengan pilihan paket yang anda pilih ketika berdiskusi dengan admin kami.</h6>
+              <p class="u-text u-text-3">Jika anda mengisi formulir ini, berarti anda sudah mengerti dan memahami mengenai syarat &amp; ketentuan yang berlaku.</p>
+              <div class="u-social-icons u-spacing-20 u-social-icons-1" data-animation-name="customAnimationIn" data-animation-duration="1000" data-animation-direction="">
+                <a class="u-social-url" target="_blank" href="https://www.instagram.com/weare.eventhorizon/" title="Event Horizon"><span class="u-icon u-icon-circle u-social-icon u-social-instagram u-text-white u-icon-1">
+        <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" ><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-4718"></use></svg>
+        <svg x="0px" y="0px" viewBox="0 0 112 112" id="svg-4718" class="u-svg-content"><path d="M55.9,32.9c-12.8,0-23.2,10.4-23.2,23.2s10.4,23.2,23.2,23.2s23.2-10.4,23.2-23.2S68.7,32.9,55.9,32.9z M55.9,69.4c-7.4,0-13.3-6-13.3-13.3c-0.1-7.4,6-13.3,13.3-13.3s13.3,6,13.3,13.3C69.3,63.5,63.3,69.4,55.9,69.4z"></path><path d="M79.7,26.8c-3,0-5.4,2.5-5.4,5.4s2.5,5.4,5.4,5.4c3,0,5.4-2.5,5.4-5.4S82.7,26.8,79.7,26.8z"></path><path d="M78.2,11H33.5C21,11,10.8,21.3,10.8,33.7v44.7c0,12.6,10.2,22.8,22.7,22.8h44.7c12.6,0,22.7-10.2,22.7-22.7 V33.7C100.8,21.1,90.6,11,78.2,11z M91,78.4c0,7.1-5.8,12.8-12.8,12.8H33.5c-7.1,0-12.8-5.8-12.8-12.8V33.7 c0-7.1,5.8-12.8,12.8-12.8h44.7c7.1,0,12.8,5.8,12.8,12.8V78.4z"></path></svg>
+      </span>
+                </a>
+                <a class="u-social-url" target="_blank" data-type="Custom" title="Website Event Horizon" href="https://the-eventhorizon.com/"><span class="u-file-icon u-icon u-social-custom u-social-icon u-text-white u-icon-2"><img src="images/1.png" alt=""></span>
+                </a>
+                <a class="u-social-url" target="_blank" data-type="Custom" title="Email The Event Horizon" href="mailto:theeventhorizon.service@gmail.com"><span class="u-file-icon u-icon u-social-custom u-social-icon u-text-white u-icon-3"><img src="images/2.png" alt=""></span>
+                </a>
+                <a class="u-social-url" target="_blank" data-type="Custom" title="Whatsapp Admin" href="https://api.whatsapp.com/send?phone=6282127180662&text=Hi,%20saya%20ingin%20menanyakan%20tentang%20pembuatan%20jasa%20web%20ini"><span class="u-file-icon u-icon u-social-custom u-social-icon u-text-white u-icon-4"><img src="images/3.png" alt=""></span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
+            <div class="u-container-layout u-container-layout-2">
+              <div class="u-align-left u-form u-form-1">
+
+              
+                <form action="assets/php/submit.php" method="post" class="u-clearfix u-form-spacing-30 u-form-vertical u-inner-form" style="padding: 10px" >
+                  <div class="u-form-email u-form-group u-form-partition-factor-2 u-form-group-1">
+                    <label for="email-319a" class="u-label u-text-white u-label-1">Email</label>
+                    <input type="email" placeholder="Akun Email" id="email" name="email" class="u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-custom-color-5 u-input-1" required="" autofocus="autofocus">
+                  </div>
+                  <div class="u-form-group u-form-name u-form-partition-factor-2 u-form-group-2">
+                    <label for="name-319a" class="u-label u-text-white u-label-2">Username</label>
+                    <input type="text" placeholder="Username" id="username" name="username" class="u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-custom-color-5 u-input-2" required="">
+                  </div>
+                  <div class="u-form-group u-form-partition-factor-2 u-form-group-3">
+                    <label for="text-7894" class="u-label u-text-white u-label-3">Nama Depan</label>
+                    <input type="text" id="namadepan" name="namadepan" class="u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-custom-color-5 u-input-3" required="required" placeholder="Nama Depan">
+                  </div>
+                  <div class="u-form-group u-form-partition-factor-2 u-form-group-4">
+                    <label for="text-28b7" class="u-label u-text-white u-label-4">Nama Belakang</label>
+                    <input type="text" placeholder="Nama Belakang" id="namabelakang" name="namabelakang" class="u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-custom-color-5 u-input-4" required="required">
+                  </div>
+                  <div class="u-form-group u-form-group-5">
+                    <label for="text-0a4e" class="u-label u-text-white u-label-5">Instansi</label>
+                    <input type="text" placeholder="SMP / SMA / Universitas / Perusahaan" id="instansi" name="instansi" class="u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-custom-color-5 u-input-5" required="required">
+                  </div>
+                  <div class="u-form-group u-form-group-6">
+                    <label for="text-84d5" class="u-label u-text-white u-label-6">Nama Project</label>
+                    <input type="text" placeholder="Website .... Project" id="namaproject" name="namaproject" class="u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-custom-color-5 u-input-6" required="required">
+                  </div>
+                  <div class="u-form-group u-form-select u-form-group-7">
+                    <label for="select-4617" class="u-label u-text-white u-label-7">Paket Yang Di Ambil</label>
+                    <div class="u-form-select-wrapper">
+                      <select id="paketutama" name="paketutama" class="u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-custom-color-5 u-input-7" required="required" placeholder="Akun Email">
+                        <option value="PLANETARY Rp150.000">PLANETARY Rp150.000</option>
+                        <option value="STELLAR Rp250.000">STELLAR Rp250.000</option>
+                        <option value="GALACTIC Rp500.000">GALACTIC Rp500.000</option>
+                      </select>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="12" version="1" class="u-caret"><path fill="currentColor" d="M4 8L0 4h8z"></path></svg>
+                    </div>
+                  </div>
+                  <div class="u-form-group u-form-select u-form-group-8">
+                    <label for="select-11ef" class="u-label u-text-white u-label-8">Paket Tambahan Yang Akan Di Ambil (Optional) </label>
+                    <div class="u-form-select-wrapper">
+                      <p><i>*Jika anda menggunakan komputer / laptop, gunakan "ctrl" untuk melakukan multiple select.</i></p>
+                      <select id="pakettambahan" name="pakettambahan[]" class="u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-custom-color-5 u-input-8" placeholder="Akun Email" size="3" multiple="multiple">
+                        <option value="Pengeditan Gambar Sederhana Rp15.000/5 foto">-Pengeditan Gambar Sederhana Rp15.000/5 foto</option>
+                        <option value="Edit Halaman Website Rp20.000/halaman">-Edit Halaman Website Rp20.000/halaman</option>
+                        <option value="Tambah Halaman Website Rp50.000/halaman">-Tambah Halaman Website Rp50.000/halaman</option>
+                        <option value="Jasa Pembuatan Konten Design Rp25.000/design">-Jasa Pembuatan Konten Design Rp25.000/design</option>
+                        <option value="Preloader Rp40.000">-Preloader Rp40.000</option>
+                        <option value="Social Media Ikon Interaktif Rp25.000">-Social Media Ikon Interaktif Rp25.000</option>
+                      </select>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="12" version="1" class="u-caret"><path fill="currentColor" d="M4 8L0 4h8z"></path></svg>
+                    </div>
+                  </div>
+                  <div class="u-form-checkbox u-form-group u-form-group-9">
+                    <input type="checkbox" id="caraorder" name="caraorder" value="Setuju" required="required">
+                    <label for="checkbox-ae60" class="u-label u-text-white u-label-9"> Saya sudah membaca &amp; memahami tata&nbsp;<a href="https://youtube.com" class="u-active-none u-border-none u-btn u-button-style u-hover-none u-none u-text-palette-1-base u-btn-1" target="_blank">cara order</a>
+                    </label>
+                  </div>
+                  <div class="u-form-checkbox u-form-group u-form-group-10">
+                    <input type="checkbox" id="kebijakanprivasi" name="kebijakanprivasi" value="Setuju" required="required">
+                    <label for="checkbox-df2c" class="u-label u-text-white u-label-10">Saya menyetujui&nbsp;<a href="https://youtube.com" class="u-active-none u-border-none u-btn u-button-style u-hover-none u-none u-text-palette-1-base u-btn-2" target="_blank" rel="nofollow">kebijakan privasi&nbsp;</a>
+                      <span style="background-color: #001372; color: #ffffff ; font-family: 'Open Sans',sans-serif; font-size: 1rem;">yang berlaku</span>
+                    </label>
+                    <br>
+                  </div>
+                  <div class="u-form-checkbox u-form-group u-form-group-11">
+                    <input type="checkbox" id="syaratketentuan" name="syaratketentuan" value="Setuju" required="required">
+                    <label for="checkbox-de2a" class="u-label u-text-white u-label-11"> Saya menyetujui&nbsp;<a href="https://youtube.com" class="u-active-none u-border-none u-btn u-button-style u-hover-none u-none u-text-palette-1-base u-btn-3" target="_blank" rel="nofollow">syarat &amp; ketentuan</a>&nbsp;<span style="color: #ffffff ; text-align: start; font-size: 1rem; background-color: #001372; font-family: 'Open Sans',sans-serif;">yang berlaku</span>
+                    </label>
+                  </div>
+                  <div class="u-align-left u-form-group u-form-submit u-form-group-12">
+                    <button type="submit" class="u-btn u-btn-submit u-button-style u-white u-btn-4" name="uploadform" id="uploadform">Submit</button>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <img class="u-image u-image-default u-image-1" src="images/Untitled-2.png" alt="" data-image-width="688" data-image-height="432">
+  </section>
+  
+</body>
+
+</html>
